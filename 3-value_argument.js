db03d8e5f02e5822774fa318509a1ccc458eb9d2
Mohammed Daba Olamilekan <mohammedolamilekan1@gmail.com>
2025-09-14 19:33:46 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,10 +4,6 @@ const firstArg = process.argv[2];
 
 if (firstArg === undefined) {
     console.log("No argument");
-}  else if (firstArg === 1) {
-    console.log("HBTN");
 }  else {
     console.log ("firstArg");
 }
-
-
