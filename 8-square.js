3756290b51a54171c71e316140ca4aea27cb7997
Mohammed Daba Olamilekan <mohammedolamilekan1@gmail.com>
2025-09-14 22:41:59 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -6,7 +6,8 @@ if (isNaN(size)) {
     console.log('Missing size');
 }   else {
     for (let i = 0; i < size; i++) {
-        console.log('x'.repeat(size));
+        console.log('X'.repeat(size));
     }
 }
+
 
