a39e625a9855b9c5a9056350ae554d18f298581d
Mohammed Daba Olamilekan <mohammedolamilekan1@gmail.com>
2025-09-14 22:00:59 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let result = '';
 for (let i = 0; i < lines.length; i++) {
     result +=lines[i] + (i < lines.length - 1 ? '\n' : '');
 }
+
 
 console.log(result);
