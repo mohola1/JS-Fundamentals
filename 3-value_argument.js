18d1be652bd4c8d062a232885a0f1432a296da7a
Mohammed Daba Olamilekan <mohammedolamilekan1@gmail.com>
2025-09-14 19:36:14 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const firstArg = process.argv;
+const firstArg = process.argv[3];
 
 if (firstArg === undefined) {
     console.log("No argument");
 }  else {
     console.log ("firstArg");
 }
-
